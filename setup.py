a089dc8df4ecad9d480fb0bd2aafd138aa80fc41
AbhijeetPhD <apimpale06@gmail.com>
2024-12-17 11:04:55 +0530

    structure created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "GenAI Project",
+    version= '0.0.0',
+    author="Abhijeet Pimpale",
+    author_email="apimpale06@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
